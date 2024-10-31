9aca97ebd0396fd66b9ab765785fcfa968de6f8a
M Haidar Hanif <git@mhaidarhanif.com>
2024-11-01 03:43:48 +0700

    feat: tiptap improve

--- app/components/demos/demo-tiptap-editor.tsx
+++ app/components/demos/demo-tiptap-editor.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import { StarterKit } from "@tiptap/starter-kit";
 import { EditorContent, type Extension, useEditor } from "@tiptap/react";
-import { TextAlign } from "@tiptap/extension-text-align";
 import { Color } from "@tiptap/extension-color";
 import { Highlight } from "@tiptap/extension-highlight";
 import { TextStyle } from "@tiptap/extension-text-style";
 import { Link } from "@tiptap/extension-link";
+import { Underline } from "@tiptap/extension-underline";
+import { TextAlign } from "@tiptap/extension-text-align";
+import { Superscript } from "@tiptap/extension-superscript";
+import { Subscript } from "@tiptap/extension-subscript";
 
 import { Separator } from "~/components/ui/separator";
 import { BlockquoteToolbar } from "~/components/toolbars/blockquote";
@@ -29,14 +32,13 @@ import { ImagePlaceholder } from "~/components/extensions/image-placeholder";
 import { ImagePlaceholderToolbar } from "~/components/toolbars/image-placeholder-toolbar";
 import { ColorHighlightToolbar } from "~/components/toolbars/color-and-highlight";
 import { LinkURLToolbar } from "~/components/toolbars/link-url";
+import { UnderlineToolbar } from "~/components/toolbars/underline";
 
 const editorMode: "simple" | "complex" = "simple";
 const isSimple = editorMode === "simple";
 
-const content = `
-<h2 class="tiptap-heading">Hello world</h2>
-<p>Try interacting with the image below</p>
-`;
+const content =
+  '<h2 style="text-align: center;">Welcome!</h2><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
 
 const extensions = [
   StarterKit.configure({
@@ -72,8 +74,12 @@ const extensions = [
       },
     },
   }),
+  Underline,
+  Link,
+  Superscript,
+  Subscript,
   Link.configure({
-    openOnClick: false,
+    openOnClick: true,
     autolink: true,
     defaultProtocol: "https",
   }),
@@ -102,17 +108,18 @@ export const DemoTiptapEditor = () => {
   }
 
   return (
-    <div className="border w-full relative rounded-md overflow-hidden pb-3">
-      <div className="w-full py-2 px-2 justify-between border-b sticky top-0 left-0 bg-background z-20">
+    <div className="border w-full rounded-md overflow-hidden pb-3 relative">
+      <div className="w-full py-2 px-2 justify-between border-b sticky top-0 left-0 bg-background z-10">
         <ToolbarProvider editor={editor}>
           <section className="flex justify-between items-start gap-4">
-            <div className="flex items-center gap-1 flex-wrap">
+            <div className="flex items-center flex-wrap">
               <UndoToolbar />
               <RedoToolbar />
-              <Separator orientation="vertical" className="h-8" />
+              <Separator orientation="vertical" className="h-8 mx-1" />
               <BoldToolbar />
               <ItalicToolbar />
               <StrikeThroughToolbar />
+              <UnderlineToolbar />
               <BulletListToolbar />
               <OrderedListToolbar />
               {!isSimple && <CodeToolbar />}
@@ -124,7 +131,7 @@ export const DemoTiptapEditor = () => {
               <LinkURLToolbar />
               <ImagePlaceholderToolbar />
             </div>
-            <div className="flex items-center gap-1 flex-wrap">
+            <div className="flex items-center flex-wrap">
               <SearchAndReplaceToolbar />
             </div>
           </section>
@@ -139,7 +146,10 @@ export const DemoTiptapEditor = () => {
         }}
         className="cursor-text min-h-[18rem]"
       >
-        <EditorContent editor={editor} className="outline-none custom-prose" />
+        <EditorContent
+          editor={editor}
+          className="outline-none prose custom-prose"
+        />
       </div>
     </div>
   );
